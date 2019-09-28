dcbdf739fde2c78c53ffc2fbd6a4078b74716280
richlum <richlum@github.com>
2019-09-28 13:54:13 -0700

    generated square image inserted into sample pdf

--- src/main/java/mypdf/richlum/net/PdfInsert.java
+++ src/main/java/mypdf/richlum/net/PdfInsert.java
@@ -7,6 +7,10 @@ import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.PdfStamper;
 import com.itextpdf.text.Image;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -38,7 +42,25 @@ public class PdfInsert {
             System.out.println("target " + target );
             image.setAnnotation(new Annotation(0, 0, 100, 100, target));
             content.addImage(image);
+
+            Image square = Image.getInstance(getBlankRectImage(200,200));
+//            image.getInstance(getBlankRectImage(200,200));
+            square.setAbsolutePosition(100,100);
+            content.addImage(square);
+
         }
         stamper.close();
+    }
+    private byte[] getBlankRectImage(int width, int height) throws IOException {
+        BufferedImage image = new BufferedImage(width,height,BufferedImage.TYPE_INT_RGB);
+        Graphics2D graphics = image.createGraphics();
+        graphics.setBackground(Color.WHITE);
+        graphics.setPaint(Color.RED);
+        graphics.fill(new Rectangle(image.getWidth(),image.getHeight()));
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(image,"png",baos);
+        baos.flush();
+        return baos.toByteArray();
     }
 }
